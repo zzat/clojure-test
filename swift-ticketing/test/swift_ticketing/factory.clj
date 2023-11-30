ea1fa3618a8c1085f0f1b7eb450c4147e595f080
Tarun <tarun@nilenso.com>
2023-11-30 23:08:47 +0530

    Add factory functions for event

--- swift-ticketing/test/swift_ticketing/factory.clj
+++ swift-ticketing/test/swift_ticketing/factory.clj
@@ -1,12 +1,48 @@
 (ns swift-ticketing.factory)
 
+(defn mk-prefix [x]
+  (str x "-"))
+
+(defn mk-event-name [x]
+  (str (mk-prefix "Event") x))
+
+(defn mk-event-description [x]
+  (str (mk-event-name x) " Description"))
+
+(defn mk-event-venue [x]
+  (str (mk-event-name x) " Venue"))
+
+(defn mk-ticket-type [x]
+  (str (mk-prefix "TicketType") x))
+
+(defn mk-ticket-name [x]
+  (str (mk-prefix "Ticket") x))
+
+(defn mk-ticket-description [x]
+  (str (mk-ticket-name x) " Description"))
+
 (defn random-date []
   (apply format "%4d-%02d-%02d" [(+ 2023 (rand-int 5))
                                  (inc (rand-int 12))
                                  (inc (rand-int 28))]))
 (defn event-request []
   (let [random-id (rand-int 10000)]
-    {"name" (str "Event-" random-id)
-     "description" (str "Event-" random-id " Description")
+    {"name" (mk-event-name random-id)
+     "description" (mk-event-description random-id)
      "date" (random-date)
-     "venue" (str "Event-" random-id " Venue")}))
+     "venue" (mk-event-venue random-id)}))
+
+(defn event-with-tickets [event-id]
+  (let [ticket-type-id (java.util.UUID/randomUUID)]
+    {:event_id (str event-id)
+     :event_name (mk-event-name event-id)
+     :event_description (mk-event-description event-id)
+     :event_date (random-date)
+     :venue (mk-event-venue event-id)
+     :ticket_type (mk-ticket-type event-id)
+     :ticket_type_id (str ticket-type-id)
+     :seat_type "General"
+     :ticket_count (rand-int 100)
+     :ticket_name (mk-ticket-name event-id)
+     :ticket_description (mk-ticket-description event-id)
+     :ticket_price (rand-int 10000)}))
