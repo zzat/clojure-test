859bdfa2ef2792518e52f9fb368f8cbfa44f297e
Tarun <tarun@nilenso.com>
2023-12-04 16:59:40 +0530

    Replace functions from client instead of explicit mock calls

--- swift-ticketing/test/swift_ticketing/handlers_test.clj
+++ swift-ticketing/test/swift_ticketing/handlers_test.clj
@@ -12,31 +12,23 @@
             [swift-ticketing.utils :as utils]
             [swift-ticketing.db.event :as db-event]
             [swift-ticketing.db.ticket :as ticket]
+            [swift-ticketing.client :as client]
             [swift-ticketing.specs :as specs]))
 
 (use-fixtures :each fixtures/run-migrations)
 
 (deftest create-event-test
-  (let [{:keys [db-spec test-user-id]} fixtures/test-env
-        app (fn [req] ((swift-ticketing-app db-spec) req))]
+  (let [{:keys [db-spec]} fixtures/test-env]
 
     (testing "Create event"
       (testing "with valid request"
-        (let [request (factory/event-request)
-              response (-> (mock/request :post "/event")
-                           (mock/json-body request)
-                           (mock/cookie "uid" test-user-id)
-                           app)
-              response-json (-> response
-                                :body
-                                json/read-str)
-              event-id (-> response-json
-                           (get "event_id"))
-              to-db-event (s/rename-keys request {"name" "event_name"
-                                                  "description" "event_description"
-                                                  "date" "event_date"})
-              created-event (db-event/get-event db-spec event-id)]
-          (is (= (:status response) 201))
+        (let [{:keys [request status response]} (client/create-event)
+              event-id (get response "event_id")
+              to-db-event #(s/rename-keys % {"name" "event_name"
+                                             "description" "event_description"
+                                             "date" "event_date"})
+              created-event (first (db-event/get-event db-spec event-id))]
+          (is (= status 201))
           (is ((comp not nil?) event-id) "Should return an event_id")
           (is (utils/submap? (to-db-event request) created-event)
               "Created data should match the data in request")))
@@ -45,11 +37,8 @@
         (let [event (factory/event-request)]
           (doseq [key (keys event)]
             (let [request (dissoc event key)
-                  response (-> (mock/request :post "/event")
-                               (mock/json-body request)
-                               (mock/cookie "uid" test-user-id)
-                               app)]
-              (is (= (:status response) 400)
+                  {:keys [request status response]} (client/create-event request)]
+              (is (= status 400)
                   (str "Request without '" key "' should return 400")))))))))
 
 (deftest list-events-test
@@ -58,11 +47,8 @@
 
     (testing "List events"
       (testing "with valid request"
-        (let [response (-> (mock/request :get "/event")
-                           app)
-              response-json (-> response
-                                :body
-                                json/read-str)
+        (client/create-event)
+        (let [{:keys [response status]} (client/list-events)
               event-response-keys ["event_id"
                                    "event_name"
                                    "event_description"
@@ -73,12 +59,12 @@
                               event-response-keys)
               ; valid-date? #(instance? java.util.Date (get % "event_date"))
               ]
-          (is (= (:status response) 200))
-          (is (vector? response-json))
-          (is (every? keys-present? response-json))
+          (is (= status 200))
+          (is (vector? response))
+          (is (every? keys-present? response))
 
           (testing "with query params"
-            (let [an-event (first response-json)
+            (let [an-event (first response)
                   venue (get an-event "venue")
                   date (-> an-event
                            (get "event_date")
@@ -86,17 +72,16 @@
                   req-with-query-params (fn [qp] (-> (mock/request :get "/event")
                                                      (mock/query-string qp)
                                                      app))
-                  valid-venue-resp (req-with-query-params
-                                    {:venue venue})
-                  non-existent-venue-resp (req-with-query-params
+                  valid-venue-resp (client/list-events {:venue venue})
+                  non-existent-venue-resp (client/list-events
                                            {:venue "Some non existant venue"})
-                  valid-from-resp (req-with-query-params
+                  valid-from-resp (client/list-events
                                    {:from date})
-                  invalid-from-resp (req-with-query-params
+                  invalid-from-resp (client/list-events
                                      {:from "Invalid Date"})
-                  valid-to-resp (req-with-query-params
+                  valid-to-resp (client/list-events
                                  {:to date})
-                  invalid-to-resp (req-with-query-params
+                  invalid-to-resp (client/list-events
                                    {:to "Invalid Date"})
                   response-to-json (fn [r] (-> r
                                                :body
@@ -104,7 +89,7 @@
                   valid-responses [valid-venue-resp
                                    valid-to-resp
                                    valid-from-resp]
-                  valid-responses-json (map response-to-json valid-responses)
+                  valid-responses-json (map :response valid-responses)
                   invalid-responses [invalid-to-resp
                                      invalid-from-resp]]
               (testing "(valid params)"
@@ -117,7 +102,7 @@
                 ;; invalid requests dhould return 400
                 (is (every? #(= (:status %) 400) invalid-responses))
                 ;; check for empty response
-                (is (empty? (:body (response-to-json non-existent-venue-resp))))))))))))
+                (is (empty? (:response non-existent-venue-resp)))))))))))
 
 (deftest get-event-test
   (let [{:keys [db-spec]} fixtures/test-env
